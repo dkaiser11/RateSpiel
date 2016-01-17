2acc3be150e2d8a153c6896664bed5b667861fef
David Kaiser <kaiserdavid9@gmail.com>
2016-01-17 15:14:41 +0100

    ich habe die Variable "eingabe" in "enter" geändert

--- src/org/kaiserdavid9/Ratespiel/Main.java
+++ src/org/kaiserdavid9/Ratespiel/Main.java
@@ -6,29 +6,29 @@ public class Main {
 
     public static void main(String[] args) {
         final int loesung = 45;
-        int eingabe;
+        int enter;
         int tohigh = 51;
         Scanner in = new Scanner(System.in);
 
         System.out.println("Ratespiel");
         do {
             System.out.println("Erraten sie die Zahl von 0-" + tohigh + ":");
-            eingabe = in.nextInt();
-            System.out.println("Sie haben eingegeben: " + eingabe);
+            enter = in.nextInt();
+            System.out.println("Sie haben eingegeben: " + enter);
 
 
-            if (eingabe == loesung) {
+            if (enter == loesung) {
                 System.out.println("Richtig!");
-            } else if (eingabe < 0) {
+            } else if (enter < 0) {
                 System.out.println("Ihre Eingabe ist kleiner als 0!");
-            } else if (eingabe < loesung) {
+            } else if (enter < loesung) {
                 System.out.println("Ihre Eingabe ist zu klein!");
-            } else if (eingabe > loesung) {
+            } else if (enter > loesung) {
                 System.out.println("Ihre Eingabe ist zu groß!");
-            } else if (eingabe >= tohigh) {
+            } else if (enter >= tohigh) {
                 System.out.println("Ihre Eingabe ist über " + tohigh + "!");
             }
-        } while (eingabe != loesung);
+        } while (enter != loesung);
 
         System.out.println("Gut gemacht!");
     }
